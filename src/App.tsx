4fd825c26ea77354000f654296c6b0bd23e209d0
sferey-pro <sferey.pro@gmail.com>
2026-08-01 11:03:39 +0200

    chore: corriger l'import css vers globals.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "@/pages/HomePage";
-import "./index.css";
+import "../styles/globals.css";
 
 export function App() {
 	return (
